0446b241ba015701c7d0a365f7a807c3c2da98ca
Jordan & Ryan <jordan-ryan@appacademy.io>
2013-02-13 14:30:41 -0800

    Finished Tic Tac Toe in the browser

--- TicTacToe/app.js
+++ TicTacToe/app.js
@@ -1,89 +1,111 @@
-// var game = require ('./towers_of_hanoi').TowersOfHanoi();
+function TicTacToe () {
+  return {
+    players: ['X','O'],
+    setBoard: function () {
+      this.board = [[null, null, null],
+                    [null, null, null],
+                    [null, null, null]];
+    },
+    move: function (x, y, player_mark) {
+      this.board[y][x] = player_mark;
+    },
+    validMove: function (x, y) {
+      if (x > 2 || x < 0 || y > 2 || y < 0) {
+        return false;
+      } else if (this.board[y][x] !== null) {
+        return false;
+      } else {
+        return true;
+      }
+    },
+    winner: function () {
+      var flat_board = this.board[0].concat(this.board[1]).concat(this.board[2])
+      var winning_lines = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
+                     [0, 3, 6], [1, 4, 7], [2, 5, 8],
+                     [0, 4, 8], [2, 4, 6]];
 
-var TowersOfHanoi = function () {
-  var game = {
-    board: undefined,
-    setBoard: function() {
-      this.board = [[3,2,1],[],[]]
-    },
-    move: function(from, to) {
-      var disc = this.board[from].pop();
-      this.board[to].push(disc);
-    },
-    validMove: function(from, to) {
-      if (from > 2 || from < 0 || to > 2 || to < 0) {
-        return false;
+      var winner = false;
+
+      function threeInARow (positions, index, array) {
+        var line = flat_board[positions[0]] + flat_board[positions[1]] + flat_board[positions[2]]
+        if (line === 'XXX') {
+          winner = 'X';
+        } else if (line === 'OOO') {
+          winner = 'O';
+        }
       }
-      else if (from == to) {
-        return false;
+
+      winning_lines.forEach(threeInARow);
+
+      if (flat_board.indexOf(null) == -1) {
+        winner = 'tie';
       }
-      else if (this.board[to][this.board[to].length - 1] < this.board[from][this.board[from].length - 1]) {
-        return false;
-      }
-      else {
-        return true;
-      }
-    },
-    gameOver: function() {
-      if (this.board[1].length == 3 || this.board[2].length == 3) {
-        return true;
-      }
-      else {
-        return false;
-      }
+
+      return winner;
     }
-  }
+  };
+};
 
-  return game;
-}
-
-
-function playTowers(game) {
-  var playGame = {
-    getMove: function() {
-      var move = prompt("What's your move? (from, to)");
-      move = move.split(', ');
-      for (var i = 0; i < move.length; i++) {
-        move[i] = parseInt(move[i]) - 1;
-      }
-      return move;
+function TicTacToeGame (game) {
+  return {
+    printBoard: function () {
+      println(game.board[0].join(" | "));
+      println("-------");
+      println(game.board[1].join(" | "));
+      println("-------");
+      println(game.board[2].join(" | "));
     },
-    printBoard: function() {
-      println("Tower 1:")
-      println(game.board[0].join(', '));
-      println("Tower 2:")
-      println(game.board[1].join(', '));
-      println("Tower 3:")
-      println(game.board[2].join(', '));
-    },
-    printInvalidMove: function() {
-      alert("Invalid move. Please try again.");
-    },
-    printWinner: function() {
+    printWinner: function (winner) {
       this.printBoard();
-      println("You won!");
+      if (winner == 'tie') {
+        println("It's a tie!");
+      } else {
+        println("Player " + winner + " won!");
+      }
     },
-    clearScreen: function() {
+    printInvalid: function () {
+      alert("Invalid move, try again.");
+    },
+    clearScreen: function () {
       clear();
     },
-    play: function() {
+    getMove: function (player_mark) {
+      move = prompt(player_mark + ", enter your move (x, y)");
+      return move.split(", ");
+    },
+    play: function () {
       game.setBoard();
-      while (!game.gameOver()) {
-        this.printBoard();
-        var move = this.getMove();
-        if (game.validMove(move[0], move[1])) {
-          game.move(move[0], move[1]);
-        } else {
-          this.printInvalidMove();
-        }
-        this.clearScreen();
-      }
-      this.printWinner();
-    }
-  }
 
-  return playGame;
-}
+      while (true) {
+
+        for (var i = 0; i < 2; i++) {
+          this.printBoard();
+          while (true) {
+            var move = this.getMove(game.players[i]);
+            if (game.validMove(move[0], move[1])) {
+              break;
+            }
+            this.printInvalid();
+          }
+          game.move(move[0], move[1], game.players[i]);
+          this.clearScreen();
+
+          if (game.winner()) {
+            break;
+          }
+        }
+
+
+        if (game.winner()) {
+          break;
+        }
+      };
+
+      this.printWinner(game.winner());
+    }
+  };
+};
+
 
 function println(string) {
   // we'll learn about this when we talk about DOM manipulation.
@@ -95,8 +117,5 @@ function clear() {
   $('.output').html("");
 }
 
-playTowers(TowersOfHanoi()).play();
 
-// println("Loaded it up!");
-// var input = prompt("Type in your input:");
-// println(input);
+TicTacToeGame(TicTacToe()).play();
